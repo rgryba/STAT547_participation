d51c366cba8f2cbe8c732b3590cf0ac99670a211
rgryba <r.gryba@stat.ubc.ca>
2018-11-08 21:08:52 -0800

    Commit of class 106 participation.

--- cm105/R/pow.R
+++ cm105/R/pow.R
@@ -1,1 +1,7 @@
-pow <- function(x, p) x^p
+pow <- function(x, a=2) x^a
+res <- x^p
+if (showplot) {
+  p <- ggplot2::qplot(x, res)
+  print(p)
+}
+res
--- cm105/tests/testthat/test_square.R
+++ cm105/tests/testthat/test_square.R
@@ -3,10 +3,10 @@ context("Squaring non-numerics")
 test_that("At least numeric values work.", {
   num_vec <- c(0, -4.6, 3.4)
   expect_identical(square(numeric(0)), numeric(0))
-  expect_identical(squre(1:3), c(1, 4, 9))
+  expect_identical(square(1:3), c(1, 4, 9))
 })
 
 test_that("Logicals automatically convert to numeric.", {
   logic_vec <- c(TRUE, TRUE, FALSE)
-  expect_identical(squre(logic_vec), c(1,1,0))
+  expect_identical(square(logic_vec), c(1,1,0))
 })
